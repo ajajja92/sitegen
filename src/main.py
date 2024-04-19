eed5331ab51e3bb2043af4fddf6866d0be6b1428
ajajja92 <ajajja92@gmail.com>
2024-04-18 21:09:13 -0700

    Completed part I of boot.dev Nodes chapter.

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from textnode import TextNode
+
+def main():
+   Test_Node = TextNode("This is awesome!!\nLook at me finally learning to code.\nI'll be proficient in no time!!!", "Bold", https://www.boot.dev)
+   print(TextNode)
