d2bab79773cbe816a1d1c71ab2ea8da96dc114e3
ajajja92 <ajajja92@gmail.com>
2024-04-21 19:16:45 -0700

    First update to the "HTMLnode" .py file

--- htmlnode.py
+++ htmlnode.py
@@ -8,9 +8,16 @@ class HTMLNode:
     def to_html(self):
         raise NotImplementedError
     
-    def props_to_html(self):
-        pass
+    def props_to_html(self):ß
+        s = ""
+        for key, value in self.props.items():Í
+            s = s + key + "\"" + value + "\" "
+        return s
+
 
     def __repr__(self):
          return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
+class LeafNode(HTMLNode):
+    def __init__(self):
+        super().__init__()
